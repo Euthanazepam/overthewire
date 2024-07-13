f588b7a52c17f94b6098a9a21e0e7892a3cf93ed
Euthanazepam <euthanazepam@protonmail.com>
2024-07-13 20:47:48 +0000

    #update #krypton - level0, added link to task

--- krypton/level0/get-password.sh
+++ krypton/level0/get-password.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#PASSWORD=$(base64 -d <<< 'S1JZUFRPTklTR1JFQVQ=')
+# https://overthewire.org/wargames/krypton/krypton0.html
+
 PASSWORD=$(echo -n 'S1JZUFRPTklTR1JFQVQ=' | base64 --decode)
+#PASSWORD=$(base64 -d <<< 'S1JZUFRPTklTR1JFQVQ=')
 
 echo $PASSWORD
