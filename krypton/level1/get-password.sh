c13bda982932d74603e041f08e89dd8d64534053
Euthanazepam <euthanazepam@protonmail.com>
2024-07-13 20:48:55 +0000

    #update #krypton - level1-2, check for task file existence

--- krypton/level1/get-password.sh
+++ krypton/level1/get-password.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-scp -P 2231 krypton1@krypton.labs.overthewire.org:/krypton/krypton1/krypton2 .
+# https://overthewire.org/wargames/krypton/krypton1.html
+
+if [ ! -f krypton2 ]; then
+  scp -P 2231 krypton1@krypton.labs.overthewire.org:/krypton/krypton1/krypton2 .
+fi
 
 PASSWORD=$(cat krypton2 | tr 'A-Za-z' 'N-ZA-Mn-za-m')
 
--- krypton/level2/get-password.sh
+++ krypton/level2/get-password.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-scp -P 2231 krypton2@krypton.labs.overthewire.org:/krypton/krypton2/krypton3 .
+# https://overthewire.org/wargames/krypton/krypton2.html
+
+if [ ! -f krypton3 ]; then
+  scp -P 2231 krypton2@krypton.labs.overthewire.org:/krypton/krypton2/krypton3 .
+fi
 
 PASSWORD=$(cat krypton3 | tr 'M-ZA-L' 'A-Z')
 
